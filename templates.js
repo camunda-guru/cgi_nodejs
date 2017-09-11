3c31caf53bf81e26ded1b006a70c8266c42e894c
eswaribala <Parameswaribala@gmail.com>
2017-09-11 15:58:58 +0530

    Add files via upload

---
+++ templates.js
@@ -0,0 +1,12 @@
+/**
+ * Created by BALASUBRAMANIAM on 11/09/2017.
+ */
+var customer={
+
+    customerName:'Anoop',
+    billAmount:39569
+
+}
+
+let message=`Dear ${customer.customerName}, kindly clear the bill amount ${customer.billAmount}`;
+console.log(message);
